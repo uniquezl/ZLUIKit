1ead1af7605190f807cf666cc13e750be0be9425
uniquezl <zhulin_8256@sina.cn>
2018-05-21 10:24:43 +0800

    Initial Commit

---
+++ ZLUikitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZLUikitTest
+//
+//  Created by zhulin on 2018/5/21.
+//  Copyright © 2018年 zhulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
